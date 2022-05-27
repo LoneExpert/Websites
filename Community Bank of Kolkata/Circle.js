a81e2d5e79f5c7a59a207ed84d0e17ef23dc59f2
Aryan <aryan0307.be21@chitkara.edu.in>
2022-05-27 11:47:17 +0530

    first website

---
+++ Community Bank of Kolkata/Circle.js
@@ -0,0 +1,6 @@
+function area(r){
+
+		var =  area(5);
+		var total= 3.14*r
+		var Final = prompt(total);
+}
---
+++ Community Bank of Kolkata/docu.js
@@ -0,0 +1,5 @@
+var a,b,c
+var a= parseInt(prompt("Enter No 1-:"));
+var b= parseInt(prompt("Enter No 2-:"));
+var c=a+b
+document.write(c)
